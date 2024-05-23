b2c2663b7064e13169ea86a31caa662af0f920b6
analopez03 <159544230+analopez03@users.noreply.github.com>
2024-05-23 13:37:49 +0200

    CreateEmpelado1 Main.java
    Creado empleado 1

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Instantanea 1
+		Empleado e1 = new Empleado("Juan", "Gomez", 1200.0);
 	}
 
 }
