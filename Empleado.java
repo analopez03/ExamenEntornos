0f73086abff076bebfd7a0a366ba9cec5b3de53e
analopez03 <159544230+analopez03@users.noreply.github.com>
2024-05-23 13:33:16 +0200

    addConstructoresGetterSetterEmpleado.java
    Añadido los constructores y los getters y setters

--- Empleado.java
+++ Empleado.java
@@ -7,4 +7,40 @@ public class Empleado {
   private String apellido;
   //Instantanea 3
   private Double salario;
+  //Instantanea 4
+  public Empleado() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Empleado(String nombre, String apellido, Double salario) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.salario = salario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
 }
